e4b3179eb3f506061fc4b3e0eea041df543450c2
Stefan Sechelmann <sechel@math.tu-berlin.de>
2008-08-13 11:12:48 +0000

    git-svn-id: svn+ssh://thales.math.tu-berlin.de/net/dgd/sechel/svn/Matheon@858 986badc2-2932-0410-96f0-8e0caa59e138

--- src/minimalsurface/frontend/MinimalSurfaces.java
+++ src/minimalsurface/frontend/MinimalSurfaces.java
@@ -17,7 +17,7 @@ import util.debug.DBGTracer;
 
 
 /**
- * The main class for the Koebe polyhedron editor.
+ * The main class for the minimal surfaces editor.
  * <p>
  * Copyright 2005 <a href="http://www.sechel.de">Stefan Sechelmann</a>
  * <a href="http://www.math.tu-berlin.de/geometrie">TU-Berlin</a> 
